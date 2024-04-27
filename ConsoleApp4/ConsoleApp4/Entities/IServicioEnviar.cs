01d050b8ed6cca4f20d3000685e8016707acdace
Theneillsaaco <157244812+Theneillsaaco@users.noreply.github.com>
2024-04-27 16:58:24 -0400

    proyecto 2

---
+++ ConsoleApp4/ConsoleApp4/Entities/IServicioEnviar.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp4.Entities
+{
+    public interface IServicioEnviar
+    {
+        public abstract void EnviarCorreoElectronico(string mensaje);
+
+        public abstract void ImprimirDatos();
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Entities/Persona.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp4.Entities
+{
+    public abstract class Persona
+    {
+        public string? nombre { get; set; }
+
+        public int edad { get; set; }
+
+        public string? direccion { get; set; }
+
+        public string? correoElectronico { get; set; }
+    }
+}
