525a0eb3e5168f63e9985d6f627411b45fb22b07
Theneillsaaco <157244812+Theneillsaaco@users.noreply.github.com>
2024-05-05 15:41:57 -0400

    program1

---
+++ ConsoleApp5/ConsoleApp5/Entities/Club.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp5.Entities
+{
+    public class Club
+    {
+        private Socios socio1;
+        private Socios socio2;
+        private Socios socio3;
+
+        public Club(Socios socio1, Socios socio2, Socios socio3)
+        {
+            this.socio1 = socio1;
+            this.socio2 = socio2;
+            this.socio3 = socio3;
+        }
+
+        public void PrintSocioOld()
+        {
+            Socios socioMayor = socio1;
+
+            if (socio2.GetOld() > socioMayor.GetOld())
+            {
+                socioMayor = socio2;
+            }
+
+            if (socio3.GetOld() > socioMayor.GetOld())
+            {
+                socioMayor = socio3;
+            }
+
+            Console.WriteLine("El socio con mayor antigüedad es: " + socioMayor.GetName() + " con " + socioMayor.GetOld() + " años.");
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Entities/Socios.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp5.Entities
+{
+    public class Socios
+    {
+        private string? Name { get; set; }
+        private int Old { get; set; }
+
+        public void Socio(string name, int old)
+        {
+            this.Name = name;
+            this.Old = old;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public int GetOld()
+        {
+            return Old;
+        }
+    }
+}
