e139c6920bfebee5ddfd0764ae288822379e717c
Theneillsaaco <157244812+Theneillsaaco@users.noreply.github.com>
2024-05-11 14:45:25 -0400

    Practica OCP

---
+++ ConsoleApp9/ConsoleApp9/Entities/TaxCalculator.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp9.Entities
+{
+    public class TaxCalculator
+    {
+        public decimal Calculate(decimal income, decimal deduction, string country)
+        {
+            decimal taxAmount = 0;
+
+            Dictionary<string, decimal> taxRates = new Dictionary<string, decimal>();
+
+            //Paises y impuestos
+            taxRates.Add("India", 0.30m);
+            taxRates.Add("USA", 0.25m);
+            taxRates.Add("UK", 0.20m);
+
+            decimal taxableIncome = income - deduction;
+
+            if (taxRates.ContainsKey(country))
+            {
+                taxAmount = taxableIncome * taxRates[country];
+            }
+            else
+            {
+                //Mensaje de error
+                Console.WriteLine("El país no está admitido");
+            }
+
+            return taxAmount;
+        }
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp9.Entities;
+
+//Test
+
+TaxCalculator taxCalculator = new TaxCalculator();
+
+
+#region"Datos"
+
+decimal income = 50000m;
+decimal deduction = 10000m;
+string country = "India";
+
+#endregion
+
+decimal taxAmount = taxCalculator.Calculate(income, deduction, country);
+Console.WriteLine($"Impuesto a pagar: {taxAmount}");
+
