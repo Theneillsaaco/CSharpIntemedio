c9b9ea4cebb03f2035a5650eff7ebe4d7f434800
Theneillsaaco <157244812+Theneillsaaco@users.noreply.github.com>
2024-05-18 20:01:59 -0400

    Practica DI

---
+++ ConsoleApp13/ConsoleApp13/Entities/CustomerService.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp13.Entities
+{
+    public class CustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+    }
+}
