2df907d13949735f8cc848fe03aa66ab167a528c
Theneillsaaco <157244812+Theneillsaaco@users.noreply.github.com>
2024-05-28 22:21:14 -0400

    Crear estructura de proyecto

---
+++ Restaurante/Restaurante.Domain/Entities/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using Restaurante.Domain.Core.EntityBase;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restaurante.Domain.Entities;
+
+public partial class Cliente : Person
+{
+    [Key]
+    public int IdCliente { get; set; }
+
+    public string Telefono { get; set; }
+
+    public string Email { get; set; }
+
+    public virtual ICollection<Pedido> Pedidos { get; set; } = new List<Pedido>();
+}
---
+++ Restaurante/Restaurante.Domain/Entities/Empleado.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurante.Domain.Entities;
+
+public partial class Empleado
+{
+    [Key]
+    public int IdEmpleado { get; set; }
+
+    public string Nombre { get; set; }
+
+    public string Cargo { get; set; }
+}
